12aead8f5a18c2658d932cb4d904c758f16aa8c4
Alex <alex.barboza@usp.br>
2021-01-10 18:31:20 -0300

    create breadth firts search for undirected graph

--- main.c
+++ main.c
@@ -22,30 +22,108 @@ void print_element(void *data)
 
 void print_vertex(vertex_t *vertex)
 {
-	printf("%s | ", (char *) vertex->data);
+	vertex_data_t *data =  (vertex_data_t *) vertex->data;
+	printf("%s | ", data->label);
+}
+
+void explore_vertex(vertex_t *vertex)
+{
+	vertex_data_t *data =  (vertex_data_t *) vertex->data;
+	data->visited = true;
+	print_vertex(vertex);
+}
+
+bool is_explored(vertex_t *vertex)
+{
+	vertex_data_t *data =  (vertex_data_t *) vertex->data;
+	return data->visited;
+}
+
+void breadth_first_search(graph_t *graph, vertex_t *start_vertex)
+{
+	/* mark starting vertex as explored */
+	explore_vertex(start_vertex);
+
+	/* create queue and initialize it with start vertex */
+	queue_t *queue = queue_create();
+	queue_insert(queue, start_vertex);
+
+	int i = 0;
+	int degree_v = 0;
+	vertex_t **neighbors = NULL;
+	while (!queue_is_empty(queue)) {
+		vertex_t *v = (vertex_t *) queue_remove(queue);
+
+		/* for each edge in adjacency list of current vertex v, do */
+		degree_v = v->degree;
+		neighbors = vertex_adjacent(v, graph);
+		for (i = 0; i < degree_v; i++) {
+
+			/* if some neighbor of v is not explored, then */
+			if (!is_explored(neighbors[i])) {
+				/* mark the vertex as explore */
+				explore_vertex(neighbors[i]);
+
+				/* insert the current neighbor at the end of our queue */
+				queue_insert(queue, neighbors[i]);
+			}
+		}
+	}
+
 }
 
 int main(void)
 {
 	int i = 0;
-	queue_t *queue = queue_create();
+	graph_t *graph = graph_create();
 
-	srand(time(NULL));
-	for (i = 0; i < 30; i++) {
-		int *data = (int *) malloc(sizeof(int));
-		*data = rand() % 100;
-		printf("%d inserting %d\n", i, *data);
-		queue_insert(queue, data);
-	}
+	vertex_data_t *data1 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data1->visited = false;
+	data1->label = "s";
 
-	for (i = 0; i < 30; i++) {
-		int *data = (int *) queue_remove(queue);
-		print_element(data);
-		free(data);
-		printf(" [%d] \n", queue->size);
-	}
+	vertex_data_t *data2 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data2->visited = false;
+	data2->label = "a";
 
-	queue_free(queue);
+	vertex_data_t *data3 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data3->visited = false;
+	data3->label = "b";
 
+	vertex_data_t *data4 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data4->visited = false;
+	data4->label = "c";
+
+	vertex_data_t *data5 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data5->visited = false;
+	data5->label = "d";
+
+	vertex_data_t *data6 = (vertex_data_t *) malloc(sizeof(vertex_data_t));
+	data6->visited = false;
+	data6->label = "e";
+
+	vertex_t *vs = vertex_create(data1);
+	vertex_t *va = vertex_create(data2);
+	vertex_t *vb = vertex_create(data3);
+	vertex_t *vc = vertex_create(data4);
+	vertex_t *vd = vertex_create(data5);
+	vertex_t *ve = vertex_create(data6);
+
+	vertex_insert_at_graph(vs, graph);
+	vertex_insert_at_graph(va, graph);
+	vertex_insert_at_graph(vb, graph);
+	vertex_insert_at_graph(vc, graph);
+	vertex_insert_at_graph(vd, graph);
+	vertex_insert_at_graph(ve, graph);
+
+	edge_insert(vs, vb, graph);
+	edge_insert(vs, ve, graph);
+	edge_insert(vb, vc, graph);
+	edge_insert(vc, va, graph);
+	edge_insert(va, ve, graph);
+	edge_insert(ve, vd, graph);
+	edge_insert(vc, vd, graph);
+
+	breadth_first_search(graph, vs);
+	graph_free(graph);
 	return 0;
 }
